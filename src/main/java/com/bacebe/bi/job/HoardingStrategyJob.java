1d479bd21305000c9414a323732b9e367674d08d
rico <lauginvid@aliyun.com>
2023-06-26 14:50:25 +0800

    充值、提现、划转、数据处理

---
+++ src/main/java/com/bacebe/bi/job/HoardingStrategyJob.java
@@ -0,0 +1,30 @@
+package com.bacebe.bi.job;
+
+import com.bacebe.bi.sink.MongodbSink;
+import com.bacebe.bi.source.RocketSource;
+import org.apache.flink.api.java.utils.ParameterTool;
+import org.apache.flink.streaming.api.datastream.DataStreamSource;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.functions.sink.SinkFunction;
+
+import java.util.HashMap;
+
+
+public class HoardingStrategyJob {
+
+
+
+    public static void main(String args[]) throws Exception {
+        StreamExecutionEnvironment streamExecutionEnvironment = StreamExecutionEnvironment.getExecutionEnvironment();
+
+        HashMap<String, String> stringStringHashMap = new HashMap<>();
+        streamExecutionEnvironment.getConfig().setGlobalJobParameters(ParameterTool.fromMap(stringStringHashMap));
+        // 获取socket输入数据
+        RocketSource rocketSource=new RocketSource("127.0.0.1",9876,"BI-HOARDING","BI-HOARDING");
+        DataStreamSource<Object> textStream = streamExecutionEnvironment.addSource(rocketSource);
+        SinkFunction sink = new MongodbSink("127.0.0.1",27017,"bi","hoarding");
+        textStream.addSink(sink);
+        // 触发任务执行
+        streamExecutionEnvironment.execute("hoardingJob");
+    }
+}
