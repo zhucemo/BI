4979ce56b7642ce06c17fc3b9e2fa6703e3bd496
aaron <aaron.zhang@confti.club>
2023-06-26 17:44:25 +0800

    grid

---
+++ src/main/java/com/bacebe/bi/job/GridStrategyJob.java
@@ -0,0 +1,31 @@
+package com.bacebe.bi.job;
+
+import com.bacebe.bi.sink.MongodbSink;
+import com.bacebe.bi.sink.MongodbUpsertSink;
+import com.bacebe.bi.source.RocketSource;
+import org.apache.flink.api.java.utils.ParameterTool;
+import org.apache.flink.streaming.api.datastream.DataStreamSource;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.functions.sink.SinkFunction;
+
+import java.util.HashMap;
+
+
+public class GridStrategyJob {
+
+
+
+    public static void main(String args[]) throws Exception {
+        StreamExecutionEnvironment streamExecutionEnvironment = StreamExecutionEnvironment.getExecutionEnvironment();
+
+        HashMap<String, String> stringStringHashMap = new HashMap<>();
+        streamExecutionEnvironment.getConfig().setGlobalJobParameters(ParameterTool.fromMap(stringStringHashMap));
+        // 获取socket输入数据
+        RocketSource rocketSource=new RocketSource("127.0.0.1",9876,"BI_GRID","BI_GRID");
+        DataStreamSource<Object> textStream = streamExecutionEnvironment.addSource(rocketSource);
+        SinkFunction sink = new MongodbUpsertSink("127.0.0.1",27017,"bi","grid");
+        textStream.addSink(sink);
+        // 触发任务执行
+        streamExecutionEnvironment.execute("gridJob");
+    }
+}
---
+++ src/main/java/com/bacebe/bi/sink/MongodbUpsertSink.java
@@ -0,0 +1,83 @@
+package com.bacebe.bi.sink;
+
+
+import com.alibaba.fastjson2.JSON;
+import com.mongodb.MongoClient;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.model.UpdateOptions;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.flink.configuration.Configuration;
+import org.apache.flink.streaming.api.functions.sink.RichSinkFunction;
+import org.bson.BsonDocument;
+import org.bson.Document;
+import org.bson.conversions.Bson;
+
+
+@Slf4j
+public class MongodbUpsertSink extends RichSinkFunction<String> {
+
+
+    private transient MongoClient mongoClient;
+
+    private String host,database,collection;
+    private int port;
+
+    public MongodbUpsertSink(String host, int port, String database, String collection){
+        this.host=host;
+        this.database=database;
+        this.collection=collection;
+        this.port=port;
+    }
+
+    @Override
+    public void open(Configuration parameters) throws Exception {
+        super.open(parameters);
+        openDB();
+    }
+
+    @Override
+    public void invoke(String bean, Context context) throws Exception {
+        try {
+            log.info("sink bean:{}",bean);
+            if(mongoClient==null){
+                openDB();
+            }
+            MongoDatabase db = mongoClient.getDatabase(database);
+            MongoCollection<Document> t = db.getCollection(collection);
+            Document document = new Document();
+            document.putAll(JSON.parseObject(bean));
+            Document filter = new Document();
+            filter.put("id", document.get("id"));
+            UpdateOptions updateOptions = new UpdateOptions();
+            updateOptions.upsert(true);
+            t.updateOne(filter, document, updateOptions);
+
+        } catch (Exception e) {
+            if (null != mongoClient) {
+                mongoClient.close();
+                mongoClient = null;
+                System.out.println("里面关闭啦！");
+            }
+            e.printStackTrace();
+        }
+    }
+
+    private void openDB(){
+        mongoClient = new MongoClient(host, port);
+    }
+
+    @Override
+    public void close() throws Exception {
+        super.close();
+        try {
+            if (null != mongoClient) {
+                mongoClient.close();
+                mongoClient = null;
+                System.out.println("关闭啦！");
+            }
+        } catch (Exception e) {
+//            e.printStackTrace();
+        }
+    }
+}
